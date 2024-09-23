720be9dc7495c312071380bfc2882188c6dd1299
Harish Chinnappan <harish.chinnappan@aroopatech.com>
2024-09-23 20:38:37 +0530

    Messafe adasd

--- src/WebReceiver.js
+++ src/WebReceiver.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 const WebViewReceiver = ({ setHtml }) => {
+  const [message, setMessage] = useState('')
   useEffect(() => {
     const handleMessage = (event) => {
       // Ensure the message is from the expected source (e.g., add security checks)
@@ -10,6 +11,7 @@ const WebViewReceiver = ({ setHtml }) => {
           console.log('Received message from RN:', data)
           if (typeof data.value.value === 'string') {
             setHtml(data.value.value) // Set the message in state (you can adjust what you display)
+            setMessage(data.value.value)
           }
         } catch (error) {
           console.error('error')
@@ -24,7 +26,7 @@ const WebViewReceiver = ({ setHtml }) => {
     }
   }, [])
 
-  return <div></div>
+  return <div>harish {message}</div>
 }
 
 export default WebViewReceiver
