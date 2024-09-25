354f47d66b19a83d0ef1eb9247df8bb8195911d4
Harish Chinnappan <harish.chinnappan@aroopatech.com>
2024-09-25 19:08:10 +0530

    dsfs

--- src/HtmlRenderer.js
+++ src/HtmlRenderer.js
@@ -2,30 +2,35 @@ import React, { useEffect, useState } from 'react'
 import './LexicalEditor/index.css'
 
 const HtmlRenderer = () => {
-  const [html, setHtml] = useState('')
+  const [html, setHtml] = useState('') // State to hold HTML content
+
   useEffect(() => {
     const handleMessage = (event) => {
       if (event?.data) {
         try {
-          const data = JSON.parse(event.data)
+          const data = JSON.parse(event.data) // Parse the message received from React Native
           if (data?.key) {
-            setHtml(data.key)
+            setHtml(data.key) // Update the HTML content in state
           } else {
             console.warn('Received message does not contain expected value')
           }
-          setMessage(event.data)
         } catch (error) {
-          console.error('Error parsing message from RN:', error)
+          console.error('Error parsing message from React Native:', error)
         }
       }
     }
+
+    // Add event listeners to receive messages from React Native
     document.addEventListener('message', handleMessage)
     window.addEventListener('message', handleMessage)
+
     return () => {
+      // Clean up the event listeners
       document.removeEventListener('message', handleMessage)
       window.removeEventListener('message', handleMessage)
     }
   }, [])
+
   return (
     <div className="editor-shell" dangerouslySetInnerHTML={{ __html: html }} />
   )
